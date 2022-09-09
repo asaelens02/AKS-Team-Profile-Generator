7c8718b8291dfd6c17bc1b908535bf3bd8b8b422
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-09 11:53:54 -0500

    Run Engineer tests/fix 3 fails

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -10,3 +10,38 @@ test ('creates engineer object', () =>{
     expect (engineer.git).toBe('http://github.com/asaelens02');
 
 });
+test ('get engineer name',() => {
+
+    const engineer = new Engineer('Billy', 'Billy@Jean.com','0002', 'http://github.com/asaelens02');
+//had trouble with this line, ref docs for toEqual and stringContaining helped 
+    expect (engineer.getName()).toEqual(expect.stringContaining('Billy'));
+});
+
+test ('get engineer email',() =>{
+
+    const engineer = new Engineer('Billy', 'Billy@Jean.com','0002', 'http://github.com/asaelens02');
+    expect (engineer.getEmail()).toEqual(expect.stringContaining('Billy@Jean.com'));
+});
+
+test ('get engineer ID',() =>{
+
+    const engineer = new Engineer('Billy', 'Billy@Jean.com','0002', 'http://github.com/asaelens02');
+
+    expect (engineer.getID()).toEqual(expect.stringContaining('0002'));
+});
+
+test ('get engineer github', () =>{
+
+    const engineer = new Engineer('Billy', 'Billy@Jean.com','0002', 'http://github.com/asaelens02');
+
+    expect (engineer.getGit()).toEqual(expect.stringContaining('http://github.com/asaelens02'));
+});
+
+test ('get engineer role',() => {
+
+    const engineer = new Engineer('Billy', 'Billy@Jean.com','0002', 'http://github.com/asaelens02');
+
+    expect (engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
+})
+
+//passes all tests
