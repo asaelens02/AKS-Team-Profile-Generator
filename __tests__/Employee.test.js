d5214c7472b8aa707d86abc20b66e7c012f4f830
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-09 09:41:11 -0500

    Employee name/email tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,10 +2,24 @@ const Employee = require('../lib/Employee');
 
 test ('creates employee object',() =>{
 
-    const employee = new Employee('Jane Doe', 'Jane@Doe.com', '0001');
+    const employee = new Employee('Jane', 'Jane@Doe.com', '0001');
 
-    expect (employee.name).toBe('Jane Doe');
+    expect (employee.name).toBe('Jane');
     expect (employee.email).toBe('Jane@Doe.com');
     expect (employee.id).toBe('0001');
 
+});
+
+test ('get employee name',() => {
+
+    const employee = new Employee('Jane', 'Jane@Doe.com','0001');
+
+    expect (employee.getName()).toEqual(expect.stringContaining('Jane'));
+});
+
+test ('get employee email',() =>{
+
+    const employee = new Employee('Jane','Jane@Doe.com','0001');
+
+    expect (employee.getEmail()).toEqual(expect.stringContaining('Jane@Doe.com'));
 })
