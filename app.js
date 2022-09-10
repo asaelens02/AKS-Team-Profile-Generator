eed167e12ccebedd90853fa9abd11b020d2b949c
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-09 21:39:19 -0500

    app.js add manager questions

--- app.js
+++ app.js
@@ -16,3 +16,66 @@ const Manager = require('./lib/Manager');
 //Team list (as an array)
 const Team = [];
 
+//manager questions
+
+const addManager = () => {
+
+    return inquirer
+        .prompt ([{
+            type: 'input',
+            name:'name',
+            message: 'Please enter Manager name',
+            validate: (nameInput)=> {
+                if (nameInput) {
+                    return true;
+                }else{
+                    console.log('invalid entry, please enter Manager name');
+                    return false;
+                }
+            },
+
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'Please enter Manager email address',
+            validate: (emailInput)=> {
+                if (emailInput){
+                    return true;
+                }else{
+                    console.log ('invalid entry, please enter Manager email address');
+                    return false;
+                }
+
+            },
+
+        },
+        {
+            type:'input',
+            name:'id',
+            message:'Please enter Manager ID',
+            validate: (idInput)=> {
+                if (isNaN (idInput)){
+                    console.log('invalid entry, please enter 4 digit Manager ID');
+                    return false;
+                }else{
+                    return true;
+                }
+            },
+        },
+        {
+            type:'input',
+            name:'office number',
+            message: 'Please enter Manager office number',
+            validate: (officeInput)=> {
+                if (isNaN(officeInput)){
+                    console.log ('invalid entry, please enter Manager office number');
+                    return false;
+                }else{
+                    return true;
+                }
+            },
+
+        },
+    ])
+}
